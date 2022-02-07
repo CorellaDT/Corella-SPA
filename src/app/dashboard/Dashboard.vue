4484a7c5ec3507f9404a78acc0e2a3f510d3b9e1
DenKalinovsky <denkalinovsky@gmail.com>
2022-02-07 16:30:58 +0300

    feat: checkout table, add year, month, day
    linking (year and month) to (day and day of the week)

--- src/app/dashboard/Dashboard.vue
+++ src/app/dashboard/Dashboard.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="dashboard">
+  <perfect-scrollbar ref="root" class="dashboard" @scroll="onScroll">
     <div class="dashboard-table">
       <div class="dashboard-table__header">
-        <div class="dashboard-table__header-month">
-          {{ tableDates }}
-        </div>
-        <div class="dashboard-table__header-days">
+        <div class="dashboard-table__month" v-for="dateItem in tableDatesMonthAndYear" :key="dateItem">
+          <div class="dashboard-table-header-wrap-month">
+            <div class="dashboard-table__header-month-and-year">
+              {{ new Date(dateItem).toLocaleString('en', {month: 'long'}) }}
+              <br>
+              <span>{{ new Date(dateItem).getFullYear() }}</span>
+            </div>
+          </div>
         </div>
       </div>
       <div class="dashboard-table__content">
-
+        <div class="dashboard-table-day" v-for="dateItem in tableDates" :key="dateItem">
+          <div class="dashboard-table-content-wrap-day" v-for="date in dateItem" :key="date">
+            <div class="dashboard-table__content-day">
+              {{ date.day }}
+              <br>
+              <span>{{ date.dayOfTheWeek }}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </perfect-scrollbar>
 </template>
 
 <script>
@@ -30,7 +42,7 @@ export default {
       }, {
         title: "",
         dateStart: new Date(2022, 0, 14),
-        dateEnd: new Date(2022, 0, 25)
+        dateEnd: new Date(2022, 2, 25)
       }]
     }
   },
@@ -39,6 +51,7 @@ export default {
       let minDate = this.projects[0]?.dateStart || null
       let maxDate = this.projects[0]?.dateEnd || null
 
+      // найти минимальную  и максимальную дату в проектах
       this.projects.forEach(item => {
         if (item.dateStart < minDate) {
           minDate = item.dateStart
@@ -48,24 +61,42 @@ export default {
         }
       })
 
-      // TODO: пофиксить даты
-      while (minDate <= maxDate) {
-        const monthAndYear = {
-          month: minDate.toLocaleString('en', {month: 'short'}),
-          year: minDate.getFullYear(),
-        }
-        this.tableDates.push({
-          dayOfTheWeek: minDate.toLocaleString('en', {weekday: 'short'}),
-          day: minDate.getDay(),
-          ...monthAndYear
-        })
+      // минимальная дата расширяется до начала месяца
+      minDate.setDate(1)
 
-        if (!this.tableDatesMonthAndYear.includes(monthAndYear)) {
-          this.tableDatesMonthAndYear.push(monthAndYear)
+      // получить месяцы
+      let date = new Date(minDate)
+      let dateMonth = []
+
+      while (date <= maxDate) {
+        dateMonth.push(new Date(date))
+        date.setMonth(date.getMonth() + 1)
+      }
+      this.tableDatesMonthAndYear = dateMonth
+
+      // максимальная дата расширяется до конца месяца
+      maxDate.setMonth(maxDate.getMonth() + 1)
+      maxDate.setDate(1)
+      // получить дни
+
+      let buffer = []
+      while (minDate <= maxDate) {
+        if (buffer.length !== 0 && minDate.getDate() === 1) {
+          this.tableDates.push(buffer)
+          buffer = []
         }
+        buffer.push({
+          dayOfTheWeek: minDate.toLocaleString('en', {weekday: 'short'}),
+          day: minDate.getDate()
+        })
         minDate.setDate(minDate.getDate() + 1)
       }
-    }
+      console.log(this.tableDatesMonthAndYear)
+      console.log(this.tableDates)
+    },
+    onScroll(event) {
+      // console.log(event);
+    },
   },
   mounted() {
     this.getMonthAndDays()
@@ -75,6 +106,98 @@ export default {
 
 <style scoped lang="scss">
 .dashboard {
+  overflow-x: auto;
+  border-radius: 8px;
+
+  &-table__header {
+    background: #F8F8F8;
+    display: flex;
+    //max-height: 60px;
+    border-bottom: 1px solid #E7E7E7;
+    border-radius: 2px;
+  }
+
+  &-table__month {
+    padding: 12px 0;
+    background: #F8F8F8;
+
+    min-width: 100vw;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &-table-header-wrap-month {
+    display: flex;
+  }
+
+  &-table__header-month-and-year {
+    font-family: Rubik, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 12px;
+    color: #212121;
+    padding: 0 12px;
+
+    position: -webkit-sticky;
+    position: sticky;
+    left: 0;
+    white-space: nowrap;
+  }
+
+  &-table__header-month-and-year br {
+    content: "";
+    margin-top: -7px;
+    display: block;
+  }
+
+  &-table__header-month-and-year span {
+    display: flex;
+    margin-top: 14px;
+
+    font-family: Rubik, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 16px;
+    color: #7C7B86;
+  }
+
+  &-table__content {
+    padding: 26px 12px 30px 12px;
+    display: flex;
+  }
+
+  &-table__content-day {
+    font-family: Rubik, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
+    color: #212121;
+  }
+
+  &-table__content-day span {
+    font-family: Rubik, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 16px;
+    color: #7C7B86;
+  }
+
+  &-table__content-day br {
+    content: "";
+    margin-top: 4px;
+    display: block;
+  }
+
+  &-table-day {
+    min-width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    padding-right: 40px;
+  }
 
 }
 </style>
